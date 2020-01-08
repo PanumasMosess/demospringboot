ba151210b9ab03229d2d09ff4c5960cba10351f9
Panumas Srisook <Panumas.srisook@gmail.com>
2020-01-08 19:05:26 +0700

    test commend

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 public class PersonService {
     private final PersonDao personDao;
 
+    //commend
     @Autowired
     public PersonService(@Qualifier("postgres") PersonDao personDao) {
         this.personDao = personDao;
